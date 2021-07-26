e3f04b1f0e8de1a7d1c02aff7d9fdfe06e179271
Акопян Александр Айрапетович <akopia-sasha@mail.ru>
2021-07-26 20:40:40 +0300

    Add task 1 solution

--- Task 1/Program.cs
+++ Task 1/Program.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Threading;
 
 namespace monitor
 {
 
     //Написать на C# утилиту, которая мониторит процессы Windows и "убивает" те процессы, которые работают слишком долго.
     //- На входе три параметра: название процесса, допустимое время жизни(в минутах) и частота проверки(в минутах).
-    //- Утилита запускается из командной строки.При старте она считывает три входных параметра и начинает мониторить процессы с указанной частотой.Если процесс живет слишком долго – завершает его и выдает сообщение в лог.
+    //- Утилита запускается из командной строки. При старте она считывает три входных параметра и начинает мониторить процессы с указанной частотой. Если процесс живет слишком долго – завершает его и выдает сообщение в лог.
     //Пример запуска:
     //monitor.exe notepad 5 1
     //С такими параметрами утилита раз в минуту проверяет, не живет ли процесс notepad больше пяти минут, и "убивает" его, если живет.
@@ -16,22 +19,46 @@ namespace monitor
     {
         static void Main(string[] args)
         {
-            foreach (var arg in args)
+            if (!IsInputValid(args))
             {
-                Console.WriteLine(arg);
+                Console.WriteLine("Parameters should be: [proccess name] [number of minutes of life time] [number of minutes of frequency check]");
+                return;
             }
-            MonitorProcess();
-            Console.ReadLine();
+            var procName = args[0];
+            var lifeTime = int.Parse(args[1]);
+            var checkTime = int.Parse(args[2]);
+            MonitorProcess(procName, lifeTime, checkTime);
         }
 
-        private static void MonitorProcess()
+        private static bool IsInputValid(string[] args)
+        {
+            if (args.Length != 3) return false;
+            if (!int.TryParse(args[1], out _) || !int.TryParse(args[2], out _)) return false;
+            return true;
+        }
+
+        private static void MonitorProcess(string procName, int lifeTime, int checkTime)
         {
             var allProcesses = Process.GetProcesses();
-            foreach (var proc in allProcesses)
+            var proc = allProcesses.FirstOrDefault((p) => p.ProcessName == procName);
+            if (proc == null)
             {
-                //Console.WriteLine("Process name: {0}, id: {1}", 
-                //proc.ProcessName, proc.Id);
+                Console.WriteLine("There is no process with such name");
+                return;
             }
+            var checkAmount = lifeTime / checkTime;
+            for (int i = 0; i < checkAmount && !proc.HasExited; i++)
+            {
+                Console.WriteLine("{0} is still active", procName);
+                Thread.Sleep(TimeSpan.FromMinutes(checkTime));
+            }
+            if (!proc.HasExited)
+            {
+                proc.Kill();
+                Console.WriteLine("{0} has been terminated", procName);
+            }
+            else
+                Console.WriteLine("{0} has exited before terminating", procName);
         }
     }
 }
